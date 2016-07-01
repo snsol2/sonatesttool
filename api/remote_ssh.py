e56860725b795f4de36da9783ae627b77b56f2f7
jtdream <kimjt@telcoware.com>
2016-07-01 15:35:32 +0900

    rr

--- api/remote_ssh.py
+++ api/remote_ssh.py
@@ -7,12 +7,14 @@ class remote_ssh():
     def __del__(self):
         del self.m_ssh
 
-    def ssh_connect(self, ip, user, passwd):
-        self.m_ssh.login(ip, user, passwd)
+    def ssh_connect(self, ip, port, user, passwd):
+        # self.m_ssh.login(ip, user, password=passwd, port=port, original_prompt='[> ]')
+        self.m_ssh.login(ip, user, password=passwd, port=port)
 
     def ssh_command(self, command):
         self.m_ssh.sendline(command)
-        self.m_ssh.prompt()
+        # self.m_ssh.prompt()
+        self.m_ssh.set_unique_prompt()
         return self.m_ssh.before
 
     def ssh_disconnect(self):
