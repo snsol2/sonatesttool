e756558c0672beaae2b3374909e9f06ccb0e4fa0
jtdream <kimjt@telcoware.com>
2016-07-14 19:59:14 +0900

    by yskim

--- scenario/test_scenario1.py
+++ scenario/test_scenario1.py
@@ -6,22 +6,42 @@ from api.network import NetworkTester
 from api.instance import InstanceTester
 from api.reporter import Reporter
 from api.ssh_util import SSHUtil
+from api.ssh_tailer import SSHTailer
 from api.config import ReadConfig
-import datetime
+import time
+import threading
+
 
 CONFIG_FILE = '../config/config.ini'
 
 test_network = NetworkTester(CONFIG_FILE)
 test_instance = InstanceTester(CONFIG_FILE)
+config = ReadConfig(CONFIG_FILE)
 Reporter()
 
 ssh_util = SSHUtil()
+ssh_tail = SSHTailer()
 
-tail_info = ReadConfig(CONFIG_FILE).get_openstack_info()
-# print datetime.datetime.now().time()
-# ssh_util.ssh_ping_test(tail_info, '10.10.2.93')
-ssh_util.ssh_ping_test2('', '', '10.10.2.93')
-# print datetime.datetime.now().time()
+ssh_tail.ssh_start_tailer()
+
+ssh_util.ssh_ping('instance6', '', 'instance6:network5')
+ssh_util.ssh_ping('instance6', 'instance1:network5', 'instance6:network5')
+ssh_util.onos_application_status()
+ssh_util.onos_devices_status()
+
+
+#
+for i in range(300):
+    if i == 20:
+        ssh_tail.ssh_stop_all_tailer('nok')
+
+    if i == 25:
+        break;
+
+    if i > 10:
+        print ('[%d][%d] : %s' % (i, threading.activeCount(), ssh_tail.thr_status_dic))
+
+    time.sleep(1)
 
 # SONA Test scenario
 # Reference default Config
