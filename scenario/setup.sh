08256ce08312bc594aadaf5250e73f5ead95494e
jtdream <kimjt@telcoware.com>
2016-08-02 16:36:11 +0900

    by yskim

---
+++ scenario/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install python-pip
+apt-get install python-neutronclient
+apt-get install python-novaclient
+apt-get install python-keystoneclient
+apt-get install oslo.config
+apt-get install python-pexpect
