4f205f00a129c8a3c381d9cb4dbff612dc962b68
jtdream <kimjt@telcoware.com>
2016-08-03 19:27:18 +0900

    by yskim

--- api/reporter2.py
+++ api/reporter2.py
@@ -245,7 +245,7 @@ class Reporter:
         ssh_conn = cls.ssh_connect(port, user, host, password)
         # cls.result_dic[threading.current_thread().getName()] = '[' + host + ', ' + user + ', ' + file + ']\n'
         if ssh_conn is not False:
-            Reporter.REPORT_MSG("%s, join : %s", file, ' '.join(file))
+            # Reporter.REPORT_MSG("%s, join : %s", file, ' '.join(file))
             ssh_conn.sendline('tail -f -n 0 ' + ' '.join(file))
             thr = threading.Thread(target=cls.tailer_thread, args=(ssh_conn, ))
             cls.thr_status_dic[thr.getName()] = [thr, True, type, host]
